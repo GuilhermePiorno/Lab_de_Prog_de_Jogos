fa031efed755363abf96c8767a156a1821ed27c2
GuilhermePiorno <50006421+GuilhermePiorno@users.noreply.github.com>
2023-05-04 13:18:45 -0300

    Melhorias de Labirinto
    Melhorias na implementação do labirinto, (not quite there yet)

--- MazeGen/game.py
+++ MazeGen/game.py
@@ -2,20 +2,38 @@ from PPlay.window import *
 from PPlay.sprite import *
 from generator import *
 
-janela = Window(1280, 720)
+janela = Window(1920, 1080)
 
 # Cria o sprite de Blinky e define o número de frames de sua animação.
 blinky = Sprite("./Sprites/Blinky.png", 8)
-
-
 blinky.set_position(janela.width / 2 - blinky.width / 2, janela.height / 2 - blinky.height / 2)
+
+# Chama o maze generator e armazena o labirinto em maze.txt
+# createlevel()
+
+# Acessa o labirinto criado e armazena os sprites criados em uma matriz para depois ser desenhada
+with open('maze.txt', mode='r', encoding='utf-8') as fin:
+    i = 0
+    # linha 0 e linha 32 são vazias.
+    level = [[0]*30 for _ in range(33)]
+    for linha in fin:
+        for j in range(len(linha)):
+            if linha[j] == '|':
+                wall = Sprite("Sprites/Walls.png", 6)
+                wall.set_position(j * 34, i * 34)
+                level[i][j + 1] = wall
+                # level[i][j + 1] = '|'
+        i += 1
+    # Altera o sprite das paredes para fazerem sentido
+    #for i in range(30):
+    #    for j in range(33):
+    # 990 leituras fora as comparações... do I really want to do that..?
+
 
 # O intervalo de frames parece funcionar fechado/aberto -> [Frame_Inicial, Frame Final)
 # ex: example.set_sequence_time(0, 1, _total_duration_, _loop_boolean_) mostra apenas o frame 0.
 # Duração total da animação em ms, cada animação será mostrada por t/n onde n é o número de frames de animação.
 blinky.set_sequence_time(0, 8, 100, True)
-
-
 
 teclado = Window.get_keyboard()
 facing = 'AFK'
@@ -25,7 +43,7 @@ blinky.set_sequence(0, 1, True)
 FPS = 0
 tempo = 0
 cont = 0
-createlevel()
+
 while True:
     # Leitura de Entradas
 
@@ -43,8 +61,6 @@ while True:
         facing = 'R'
         blinky.set_sequence(0, 2, True)
 
-
-
     # Movimento básico para teste de animação, descomente para habilitar.
     # blinky.move_key_x(0.1)
     # blinky.move_key_y(0.1)
@@ -58,6 +74,7 @@ while True:
         cont = 0
 
     # Exemplo equivalente ao de cima, mas utilizando for para ler as linhas do arquivo uma a uma.
+    """"
     with open('maze.txt', mode='r', encoding='utf-8') as fin:
         i = 0
         for linha in fin:
@@ -67,15 +84,20 @@ while True:
                     wall.set_position(i * 36, j * 36)
 
             i += 1
-
-
-
+    """
 
     # Inicialização de objetos
     janela.set_background_color((0, 0, 0))
+
     janela.draw_text(str(FPS), 10, janela.height - 50, size=25, color=(255, 255, 0))
 
     # Draw
+    # level[1][5].draw()
+
+    for i in range(33):
+        for j in range(28):
+            if level[i][j] != 0:
+                level[i][j].draw()
     blinky.draw()
     blinky.update()
     janela.update()
