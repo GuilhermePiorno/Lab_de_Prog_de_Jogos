bc8a14f8baeef7252410f9586cce7e55d51163ad
Mauricio Pollis <mauriciopollis@gmail.com>
2023-05-05 08:21:47 -0300

    Teste de movimentação somente em direções cardinais

--- Game/main.py
+++ Game/main.py
@@ -29,6 +29,7 @@ while True:
     # Leitura de Entradas
 
     # Mudança de animação de Blinky nas 4 direções cardinais.
+    """
     if teclado.key_pressed("UP") and facing != 'U':
         facing = 'U'
         blinky.set_sequence(6, 8, True)
@@ -41,12 +42,38 @@ while True:
     if teclado.key_pressed("RIGHT") and facing != 'R':
         facing = 'R'
         blinky.set_sequence(0, 2, True)
+    """
 
+    # Movimento do blinky, fazendo com que ele não possa se mover na diagonal
+    if(teclado.key_pressed("UP") and not(teclado.key_pressed("DOWN") or teclado.key_pressed("RIGHT") or teclado.key_pressed("LEFT"))):
+        blinky.set_sequence(6, 8, True)
+        blinky.move_key_y(1)
+    if(teclado.key_pressed("DOWN") and not(teclado.key_pressed("UP") or teclado.key_pressed("RIGHT") or teclado.key_pressed("LEFT"))):
+        blinky.set_sequence(4, 6, True)
+        blinky.move_key_y(1)
+    if(teclado.key_pressed("RIGHT") and not(teclado.key_pressed("DOWN") or teclado.key_pressed("UP") or teclado.key_pressed("LEFT"))):
+        blinky.set_sequence(0, 2, True)
+        blinky.move_key_x(1)
+    if(teclado.key_pressed("LEFT") and not(teclado.key_pressed("DOWN") or teclado.key_pressed("RIGHT") or teclado.key_pressed("UP"))):
+        blinky.set_sequence(2, 4, True)
+        blinky.move_key_x(1)
+
+    """
+    if facing == "U":
+        blinky.set_sequence(6, 8, True)
+    if facing == "D":
+        blinky.set_sequence(4, 6, True)
+    if facing == "L":
+        blinky.set_sequence(2, 4, True)
+    if facing == "R":
+        blinky.set_sequence(0, 2, True)
+    """
+    
 
 
     # Movimento básico para teste de animação, descomente para habilitar.
-    # blinky.move_key_x(0.1)
-    # blinky.move_key_y(0.1)
+    # blinky.move_key_x(1)
+    # blinky.move_key_y(1)
 
     # FPS
     tempo += janela.delta_time()
@@ -61,6 +88,10 @@ while True:
     # Inicialização de objetos
     janela.set_background_color((0, 0, 0))
     janela.draw_text(str(FPS), 10, janela.height - 50, size=25, color=(255, 255, 0))
+    janela.draw_text("LEFT:" + str(teclado.key_pressed("LEFT")), 200, 200, size=25, color=(255,255,0))
+    janela.draw_text("RIGHT:" + str(teclado.key_pressed("RIGHT")), 200, 225, size=25, color=(255,255,0))
+    janela.draw_text("UP:" + str(teclado.key_pressed("UP")), 200, 250, size=25, color=(255,255,0))
+    janela.draw_text("DOWN:" + str(teclado.key_pressed("DOWN")), 200, 275, size=25, color=(255,255,0))
 
     # Draw
     blinky.draw()
