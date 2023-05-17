94fd694b82e15b200032d4bbd72688ea221df955
Mauricio Pollis <mauriciopollis@gmail.com>
2023-05-17 19:07:58 -0300

    Pacman andando de acordo com uma lógica 'ruim'; Pacman totalmente implementado seguindo o paradigma estruturado, e não de orientação a objetos

--- Game/main.py
+++ Game/main.py
@@ -19,6 +19,7 @@ walltype = 'Curved_20'
 TesteDebugMapa = False
 buffer = 0  # Buffer para pressionar botão direcional.
 cmd = ''
+pacman_cmd = ''
 grid_toggle = False
 BGM_Toggle = True
 
@@ -48,12 +49,15 @@ blinky.set_sequence(0, 1, True)
 facing = 'AFK'
 
 # Cria o sprite de Pacman e define o número de frames de sua animação.
-pacman = Enemy(janela, level, "./Sprites/pacman.png", 8)
+pacman = Sprite("./Sprites/pacman.png", 8)
 pacman.set_position(janela.width / 2 + half_maze_width - (wall.width * 1.5 + pacman.width / 2),
                     janela.height / 2 + half_maze_height - (wall.height * 1.5 + pacman.height / 2))
 pacman.set_sequence_time(0, 8, 100, True)
 pacman.set_sequence(0, 1, True)
 pacman_facing = 'AFK'
+pacman_vx = 0
+pacman_vy = 0
+pacman_base_speed = 120
 
 # Portal_Esquerdo
 portal_esquerdo = Sprite("Sprites/Walls/" + walltype + "/Portal_L.png", 3)
@@ -92,6 +96,20 @@ while True:
     if blinky.vx > 0 and facing != 'R':
         facing = 'R'
         blinky.set_sequence(0, 2, True)
+
+    # Mudança de animação de Blinky nas 4 direções cardinais.
+    if pacman_vy < 0 and pacman_facing != 'U':
+        pacman_facing = 'U'
+        pacman.set_sequence(6, 8, True)
+    if pacman_vy > 0 and pacman_facing != 'D':
+        pacman_facing = 'D'
+        pacman.set_sequence(4, 6, True)
+    if pacman_vx < 0 and pacman_facing != 'L':
+        pacman_facing = 'L'
+        pacman.set_sequence(2, 4, True)
+    if pacman_vx > 0 and pacman_facing != 'R':
+        pacman_facing = 'R'
+        pacman.set_sequence(0, 2, True)
 
     # Código para a geração do mapa para testes.
     if not teclado.key_pressed("N") and not teclado.key_pressed("G") and not teclado.key_pressed("M"):
@@ -144,14 +162,50 @@ while True:
     blinky_newaxis_x = blinky.x - (janela.width / 2 - half_maze_width) + blinky.width / 2
     blinky_newaxis_y = blinky.y - (janela.height / 2 - half_maze_height) + blinky.height / 2
 
+    # Coordenadas do pacman em relação ao 0 da fase
+    pacman_newaxis_x = pacman.x - (janela.width / 2 - half_maze_width) + pacman.width / 2
+    pacman_newaxis_y = pacman.y - (janela.height / 2 - half_maze_height) + pacman.height / 2
+
     # Versão discretizada das coordenadas do blinky com ajuste (+1) para correspondencia a matriz "level".
     new_x = blinky_newaxis_x // wall.width + 1
     new_y = blinky_newaxis_y // wall.height + 1
+
+    # Versão discretizada das coordenadas do pacman com ajuste (+1) para correspondencia a matriz "level".
+    pacman_new_x = pacman_newaxis_x // wall.width + 1
+    pacman_new_y = pacman_newaxis_y // wall.height + 1
 
     can_go_down = level[int(new_y + 1)][int(new_x)] == 0
     can_go_up = level[int(new_y - 1)][int(new_x)] == 0
     can_go_left = level[int(new_y)][int(new_x - 1)] == 0
     can_go_right = level[int(new_y)][int(new_x + 1)] == 0
+
+    pacman_can_go_down = level[int(pacman_new_y + 1)][int(pacman_new_x)] == 0
+    pacman_can_go_up = level[int(pacman_new_y - 1)][int(pacman_new_x)] == 0
+    pacman_can_go_left = level[int(pacman_new_y)][int(pacman_new_x - 1)] == 0
+    pacman_can_go_right = level[int(pacman_new_y)][int(pacman_new_x + 1)] == 0
+
+    relative_x_pacman_blinky = blinky.x - pacman.x
+    relative_y_pacman_blinky = blinky.y - pacman.y
+
+    #ia 'burra' do pacman
+    #com essa lógica de movimentação, o pacman fica frequentemente 'preso' correndo contra paredes. Talvez implementar
+    #alguma funcionalidade que impeça ele de ficar correndo contra uma parede por mais de algum tempo máximo
+    if(abs(relative_x_pacman_blinky) > abs(relative_y_pacman_blinky)):
+        # se movimentará na direção horizontal
+        if(relative_x_pacman_blinky>0):
+            #vai para a direita
+            pacman_cmd = 'r'
+        else:
+            #vai para a esquerda
+            pacman_cmd = 'l'
+    else:
+        #se movimentará na direção vertical
+        if(relative_y_pacman_blinky>0):
+            #vai para baixo
+            pacman_cmd = 'd'
+        else:
+            #vai para cima
+            pacman_cmd = 'u'
 
     # Determina as tolerâncias de movimento (até quantos pixels errados blinky aceita para fazer curva)
     delta_x = 1
@@ -182,6 +236,45 @@ while True:
                 blinky.vx = -blinky.base_speed
                 blinky.vy = 0
 
+    
+    # Determina as tolerâncias de movimento (até quantos pixels errados pacman aceita para fazer curva)
+    x_window = (pacman_new_x - 0.5) * wall.width - delta_x < pacman_newaxis_x < (pacman_new_x - 0.5) * wall.width + delta_x
+    y_window = (pacman_new_y - 0.5) * wall.height - delta_y < pacman_newaxis_y < (pacman_new_y - 0.5) * wall.height + delta_y
+    # Movimento VERTICAL (REQUERIMENTO DE POSIÇÃO HORIZONTAL)
+    if x_window:
+        if pacman_cmd == 'd' and pacman_can_go_down:
+            pacman_cmd = ''
+            pacman_vx = 0
+            pacman_vy = pacman_base_speed
+        if pacman_cmd == 'u' and pacman_can_go_up:
+            pacman_cmd = ''
+            pacman_vx = 0
+            pacman_vy = -pacman_base_speed
+
+    # Movimento HORIZONTAL (REQUERIMENTO DE POSIÇÃO VERTICAL)
+    if y_window:
+        if pacman_cmd == 'r' and pacman_can_go_right:
+            pacman_cmd = ''
+            pacman_vx = pacman_base_speed
+            pacman_vy = 0
+        if pacman_cmd == 'l' and pacman_can_go_left:
+            pacman_cmd = ''
+            pacman_vx = -pacman_base_speed
+            pacman_vy = 0
+
+    # Checa condição de colisão de pacman com parede em x
+    if not pacman_can_go_right and pacman_vx > 0 and pacman_newaxis_x >= (pacman_new_x - 0.5) * wall.width:
+        pacman_vx = 0
+    if not pacman_can_go_left and pacman_vx < 0 and pacman_newaxis_x <= (pacman_new_x - 0.5) * wall.width:
+        pacman_vx = 0
+    pacman.x += pacman_vx * dt
+
+    # Checa condição de colisão de pacman com parede em y
+    if not pacman_can_go_up and pacman_vy < 0 and pacman_newaxis_y <= (pacman_new_y - 0.5) * wall.height:
+        pacman_vy = 0
+    if not pacman_can_go_down and pacman_vy > 0 and pacman_newaxis_y >= (pacman_new_y - 0.5) * wall.height:
+        pacman_vy = 0
+    pacman.y += pacman_vy * dt
 
     # Para debug (S to stop)
     if teclado.key_pressed("P"):
@@ -206,11 +299,6 @@ while True:
         blinky.vy = 0
     # Move blinky de acordo com sua velocidade no eixo y
     blinky.y += blinky.vy * dt
-
-    #movimento do pacman
-    pacman.move(blinky)
-    pacman.x += pacman.vx * dt
-    pacman.y += pacman.vy * dt
 
     # Checa colisão de blinky com portal esquerdo.
     if blinky_newaxis_x < 0 + wall.width / 2:  # aka: 0 + 20/2 = 10
@@ -243,13 +331,6 @@ while True:
     portal_esquerdo.draw()
     portal_esquerdo.update()
     portal_direito.draw()
-    portal_direito.update()
-    janela.draw_text(str(pacman.cmd), 30, 30, 30, color=(255,0,0))
-    janela.draw_text(str(pacman.can_go_down), 30, 60, 30, color=(255,0,0))
-    janela.draw_text(str(pacman.can_go_left), 30, 90, 30, color=(255,0,0))
-    janela.draw_text(str(pacman.can_go_right), 30, 120, 30, color=(255,0,0))
-    janela.draw_text(str(pacman.can_go_left), 30, 150, 30, color=(255,0,0))
-    janela.draw_text(str(pacman.vx), 30, 180, 30, color=(255,0,0))
-    janela.draw_text(str(pacman.vy), 30, 210, 30, color=(255,0,0))
-    
+    portal_direito.update()    
+    janela.draw_text("pacman_cmd: " + str(pacman_cmd), 30, 30, 30, color=(255,0,0))
     janela.update()
