369c95210be02b4b460af98b2770b11c829cee7b
Mauricio Pollis <mauriciopollis@gmail.com>
2023-05-22 10:07:22 -0300

    Mapa, blinky e pacman funcionando como instâncias de classes Level, Player e Enemy

--- Game/EatThis/Classes/Player.py
+++ Game/EatThis/Classes/Player.py
@@ -1,5 +1,124 @@
 from PPlay.sprite import *
 
 class Player(Sprite):
-    def __init__(self, janela, level, image_file, frames=1):
+    def __init__(self, window, level, image_file, frames=1):
         super().__init__(image_file, frames)
+        self.vx = 0
+        self.vy = 0
+        self.base_speed = 100
+        self.cmd = ''
+        self.window = window
+        self.keyboard = self.window.get_keyboard()
+        self.level = level
+        self.buffer = 0
+        self.facing = 'AFK'
+        self.maze_axis = (self.x - (window.width / 2 - level.half_maze_width) + self.width / 2, 
+                          self.y - (window.height / 2 - level.half_maze_height) + self.height / 2)
+        self.matrix_position = (self.maze_axis[0] // level.wall.width + 1, self.maze_axis[1] // level.wall.width + 1)
+
+    def move1(self):
+        # Mudança de animação de Blinky nas 4 direções cardinais.
+        if self.vy < 0 and self.facing != 'U':
+            self.facing = 'U'
+            self.set_sequence(6, 8, True)
+        if self.vy > 0 and self.facing != 'D':
+            self.facing = 'D'
+            self.set_sequence(4, 6, True)
+        if self.vx < 0 and self.facing != 'L':
+            self.facing = 'L'
+            self.set_sequence(2, 4, True)
+        if self.vx > 0 and self.facing != 'R':
+            self.facing = 'R'
+            self.set_sequence(0, 2, True)
+            
+        if self.keyboard.key_pressed("UP"):
+            self.buffer = 0
+            self.cmd = 'u'
+        if self.keyboard.key_pressed("DOWN"):
+            self.buffer = 0
+            self.cmd = 'd'
+        if self.keyboard.key_pressed("RIGHT"):
+            self.buffer = 0
+            self.cmd = 'r'
+        if self.keyboard.key_pressed("LEFT"):
+            self.buffer = 0
+            self.cmd = 'l'
+
+        # Coordenadas do pacman em relação ao 0 da fase
+        self.maze_axis = self.get_maze_axis()
+        
+        #Versão discretizada das coordenadas do pacman com ajuste (+1) para correspondencia a matriz "level".
+        self.matrix_position = self.get_matrix_position()
+        
+        can_go_down = (self.level.level[int(self.matrix_position[1] + 1)][int(self.matrix_position[0])] == 0)
+        can_go_up = (self.level.level[int(self.matrix_position[1] - 1)][int(self.matrix_position[0])] == 0)
+        can_go_left = (self.level.level[int(self.matrix_position[1])][int(self.matrix_position[0] - 1)] == 0)
+        can_go_right = (self.level.level[int(self.matrix_position[1])][int(self.matrix_position[0] + 1)] == 0)
+
+        # Determina as tolerâncias de movimento (até quantos pixels errados blinky aceita para fazer curva)
+        delta_x = 1
+        delta_y = 1
+        x_window = (self.matrix_position[0] - 0.5) * self.level.wall.width - delta_x < self.maze_axis[0] < (self.matrix_position[0] - 0.5) * self.level.wall.width + delta_x
+        y_window = (self.matrix_position[1] - 0.5) * self.level.wall.height - delta_y < self.maze_axis[1] < (self.matrix_position[1] - 0.5) * self.level.wall.height + delta_y
+        # Condição para aceitar qualquer input de movimento.
+        if self.buffer < 0.5:
+            # Movimento VERTICAL (REQUERIMENTO DE POSIÇÃO HORIZONTAL)
+            if x_window:
+                if self.cmd == 'd' and can_go_down:
+                    self.cmd = ''
+                    self.vx = 0
+                    self.vy = self.base_speed
+                if self.cmd == 'u' and can_go_up:
+                    self.cmd = ''
+                    self.vx = 0
+                    self.vy = -self.base_speed
+
+            # Movimento HORIZONTAL (REQUERIMENTO DE POSIÇÃO VERTICAL)
+            if y_window:
+                if self.cmd == 'r' and can_go_right:
+                    self.cmd = ''
+                    self.vx = self.base_speed
+                    self.vy = 0
+                if self.cmd == 'l' and can_go_left:
+                    self.cmd = ''
+                    self.vx = -self.base_speed
+                    self.vy = 0
+
+        # TODO: As vezes blinky anda demais antes de sua velocidade ser reduzida a zero,
+        #  similar ao problema de deslizamento do pong. Este problema foi remediado colocando a atualização de posição
+        #  após o check de reset e devido as velocidades baixas, mas caso a velocidade suba acima de 350 o problema retorna.
+        #  Velocidades acima de 350 são ruins de jogar, então o problema foi temporariamente ignorado.
+        
+        # Checa condição de colisão com parede em x
+        if not can_go_right and self.vx > 0 and self.maze_axis[0] >= (self.matrix_position[0] - 0.5) * self.level.wall.width:
+            self.vx = 0
+        if not can_go_left and self.vx < 0 and self.maze_axis[0] <= (self.matrix_position[0] - 0.5) * self.level.wall.width:
+            self.vx = 0
+
+        # Checa condição de colisão com parede em y
+        if not can_go_up and self.vy < 0 and self.maze_axis[1] <= (self.matrix_position[1] - 0.5) * self.level.wall.height:
+            self.vy = 0
+        if not can_go_down and self.vy > 0 and self.maze_axis[1] >= (self.matrix_position[1] - 0.5) * self.level.wall.height:
+            self.vy = 0
+
+        # Checa colisão de blinky com portal esquerdo.
+        if self.maze_axis[0] < 0 + self.level.wall.width / 2:  # aka: 0 + 20/2 = 10
+            self.x += 2 * self.level.half_maze_width - self.level.wall.width
+
+        # Checa colisão de blinky com portal direito.
+        if self.maze_axis[0] > 28 * self.level.wall.width - self.level.wall.width / 2:  # aka: 28*20 - 20/2 550
+            self.x -= 2 * self.level.half_maze_width - self.level.wall.width
+
+    def get_maze_axis(self):
+        return (self.x - (self.window.width / 2 - self.level.half_maze_width) + self.width / 2, 
+                self.y - (self.window.height / 2 - self.level.half_maze_height) + self.height / 2)
+
+    def get_matrix_position(self):
+        return ((self.x - (self.window.width / 2 - self.level.half_maze_width) + self.width / 2) // self.level.wall.width + 1, 
+                (self.y - (self.window.height / 2 - self.level.half_maze_height) + self.height / 2) // self.level.wall.width + 1)
+
+    def set_maze_axis(self):
+        pass
+
+    def set_matrix_position(self):
+        pass
--- Game/main.py
+++ Game/main.py
@@ -3,209 +3,12 @@ from PPlay.sprite import *
 from PPlay.sound import *
 from EatThis.procedural_map import *
 from EatThis.map_fill import *
-from EatThis.Classes.classes import Player
+#from EatThis.Classes.classes import Player
 from EatThis.pacman_moves import *
 import random
 from EatThis.Classes.Level import *
 from EatThis.Classes.Enemy import *
-
-def ia_pacman1():
-
-    global pacman_vx
-    global pacman_vy
-    global pacman_facing
-    global pacman_cmd
-    global maze
-
-    # Mudança de animação de Blinky nas 4 direções cardinais.
-    if pacman_vy < 0 and pacman_facing != 'U':
-        pacman_facing = 'U'
-        pacman.set_sequence(6, 8, True)
-    if pacman_vy > 0 and pacman_facing != 'D':
-        pacman_facing = 'D'
-        pacman.set_sequence(4, 6, True)
-    if pacman_vx < 0 and pacman_facing != 'L':
-        pacman_facing = 'L'
-        pacman.set_sequence(2, 4, True)
-    if pacman_vx > 0 and pacman_facing != 'R':
-        pacman_facing = 'R'
-        pacman.set_sequence(0, 2, True)
-
-    # Coordenadas do pacman em relação ao 0 da fase
-    pacman_newaxis_x = pacman.x - (janela.width / 2 - maze.half_maze_width) + pacman.width / 2
-    pacman_newaxis_y = pacman.y - (janela.height / 2 - maze.half_maze_height) + pacman.height / 2
-
-    # Versão discretizada das coordenadas do pacman com ajuste (+1) para correspondencia a matriz "level".
-    pacman_new_x = pacman_newaxis_x // maze.wall.width + 1
-    pacman_new_y = pacman_newaxis_y // maze.wall.height + 1
-
-    pacman_can_go_down = maze.level[int(pacman_new_y + 1)][int(pacman_new_x)] == 0
-    pacman_can_go_up = maze.level[int(pacman_new_y - 1)][int(pacman_new_x)] == 0
-    pacman_can_go_left = maze.level[int(pacman_new_y)][int(pacman_new_x - 1)] == 0
-    pacman_can_go_right = maze.level[int(pacman_new_y)][int(pacman_new_x + 1)] == 0
-
-    relative_x_pacman_blinky = blinky.x - pacman.x
-    relative_y_pacman_blinky = blinky.y - pacman.y
-
-    #ia 'burra' do pacman
-    #com essa lógica de movimentação, o pacman fica frequentemente 'preso' correndo contra paredes. Talvez implementar
-    #alguma funcionalidade que impeça ele de ficar correndo contra uma parede por mais de algum tempo máximo
-    if(abs(relative_x_pacman_blinky) > abs(relative_y_pacman_blinky)):
-        # se movimentará na direção horizontal
-        if(relative_x_pacman_blinky>0):
-            #vai para a direita
-            pacman_cmd = 'r'
-        else:
-            #vai para a esquerda
-            pacman_cmd = 'l'
-    else:
-        #se movimentará na direção vertical
-        if(relative_y_pacman_blinky>0):
-            #vai para baixo
-            pacman_cmd = 'd'
-        else:
-            #vai para cima
-            pacman_cmd = 'u'
-    
-    # Determina as tolerâncias de movimento (até quantos pixels errados pacman aceita para fazer curva)
-    delta_x = 1
-    delta_y = 1
-    x_window = (pacman_new_x - 0.5) * maze.wall.width - delta_x < pacman_newaxis_x < (pacman_new_x - 0.5) * maze.wall.width + delta_x
-    y_window = (pacman_new_y - 0.5) * maze.wall.height - delta_y < pacman_newaxis_y < (pacman_new_y - 0.5) * maze.wall.height + delta_y
-    # Movimento VERTICAL (REQUERIMENTO DE POSIÇÃO HORIZONTAL)
-    if x_window:
-        if pacman_cmd == 'd' and pacman_can_go_down:
-            pacman_cmd = ''
-            pacman_vx = 0
-            pacman_vy = pacman_base_speed
-        if pacman_cmd == 'u' and pacman_can_go_up:
-            pacman_cmd = ''
-            pacman_vx = 0
-            pacman_vy = -pacman_base_speed
-
-    # Movimento HORIZONTAL (REQUERIMENTO DE POSIÇÃO VERTICAL)
-    if y_window:
-        if pacman_cmd == 'r' and pacman_can_go_right:
-            pacman_cmd = ''
-            pacman_vx = pacman_base_speed
-            pacman_vy = 0
-        if pacman_cmd == 'l' and pacman_can_go_left:
-            pacman_cmd = ''
-            pacman_vx = -pacman_base_speed
-            pacman_vy = 0
-
-    # Checa condição de colisão de pacman com parede em x
-    if not pacman_can_go_right and pacman_vx > 0 and pacman_newaxis_x >= (pacman_new_x - 0.5) * maze.wall.width:
-        pacman_vx = 0
-    if not pacman_can_go_left and pacman_vx < 0 and pacman_newaxis_x <= (pacman_new_x - 0.5) * maze.wall.width:
-        pacman_vx = 0
-    pacman.x += pacman_vx * dt
-
-    # Checa condição de colisão de pacman com parede em y
-    if not pacman_can_go_up and pacman_vy < 0 and pacman_newaxis_y <= (pacman_new_y - 0.5) * maze.wall.height:
-        pacman_vy = 0
-    if not pacman_can_go_down and pacman_vy > 0 and pacman_newaxis_y >= (pacman_new_y - 0.5) * maze.wall.height:
-        pacman_vy = 0
-    pacman.y += pacman_vy * dt
-
-
-def ia_pacman2():
-    
-    global janela
-    global half_maze_height
-    global half_maze_width
-    global pacman
-    global pacman_vx
-    global pacman_vy
-    global pacman_facing
-    global pacman_base_speed
-    global pacman_cmd
-    global pacman_move_list_is_empty
-    global wall
-    global moves
-
-    # Mudança de animação de Blinky nas 4 direções cardinais.
-    if pacman_vy < 0 and pacman_facing != 'U':
-        pacman_facing = 'U'
-        pacman.set_sequence(6, 8, True)
-    if pacman_vy > 0 and pacman_facing != 'D':
-        pacman_facing = 'D'
-        pacman.set_sequence(4, 6, True)
-    if pacman_vx < 0 and pacman_facing != 'L':
-        pacman_facing = 'L'
-        pacman.set_sequence(2, 4, True)
-    if pacman_vx > 0 and pacman_facing != 'R':
-        pacman_facing = 'R'
-        pacman.set_sequence(0, 2, True)
-
-    # Coordenadas do pacman em relação ao 0 da fase
-    pacman_newaxis_x = pacman.x - (janela.width / 2 - half_maze_width) + pacman.width / 2
-    pacman_newaxis_y = pacman.y - (janela.height / 2 - half_maze_height) + pacman.height / 2
-
-    # Versão discretizada das coordenadas do pacman com ajuste (+1) para correspondencia a matriz "level".
-    pacman_new_x = pacman_newaxis_x // wall.width + 1
-    pacman_new_y = pacman_newaxis_y // wall.height + 1
-
-    pacman_can_go_down = level[int(pacman_new_y + 1)][int(pacman_new_x)] == 0
-    pacman_can_go_up = level[int(pacman_new_y - 1)][int(pacman_new_x)] == 0
-    pacman_can_go_left = level[int(pacman_new_y)][int(pacman_new_x - 1)] == 0
-    pacman_can_go_right = level[int(pacman_new_y)][int(pacman_new_x + 1)] == 0
-
-    # ia baseada em breadth search 'truncada'
-    if(pacman_move_list_is_empty):
-        moves = pacman_move_list(level, [int(pacman_new_x), int(pacman_new_y)], [int(new_x), int(new_y)])
-        pacman_move_list_is_empty = False
-    else:
-        if(len(moves) > 1):
-            pacman_cmd = moves[0].lower()
-            moves = moves[1:]
-        elif(len(moves) == 1):
-            pacman_cmd = moves[0].lower()
-            moves = []
-            pacman_move_list_is_empty = True
-
-    # pacman aleatório: pacman_cmd = random.choice(['r', 'l', 'u', 'd'])
-
-    # Determina as tolerâncias de movimento (até quantos pixels errados pacman aceita para fazer curva)
-    delta_x = 1
-    delta_y = 1
-    x_window = (pacman_new_x - 0.5) * wall.width - delta_x < pacman_newaxis_x < (pacman_new_x - 0.5) * wall.width + delta_x
-    y_window = (pacman_new_y - 0.5) * wall.height - delta_y < pacman_newaxis_y < (pacman_new_y - 0.5) * wall.height + delta_y
-    # Movimento VERTICAL (REQUERIMENTO DE POSIÇÃO HORIZONTAL)
-    if x_window:
-        if pacman_cmd == 'd' and pacman_can_go_down:
-            pacman_cmd = ''
-            pacman_vx = 0
-            pacman_vy = pacman_base_speed
-        if pacman_cmd == 'u' and pacman_can_go_up:
-            pacman_cmd = ''
-            pacman_vx = 0
-            pacman_vy = -pacman_base_speed
-
-    # Movimento HORIZONTAL (REQUERIMENTO DE POSIÇÃO VERTICAL)
-    if y_window:
-        if pacman_cmd == 'r' and pacman_can_go_right:
-            pacman_cmd = ''
-            pacman_vx = pacman_base_speed
-            pacman_vy = 0
-        if pacman_cmd == 'l' and pacman_can_go_left:
-            pacman_cmd = ''
-            pacman_vx = -pacman_base_speed
-            pacman_vy = 0
-
-    # Checa condição de colisão de pacman com parede em x
-    if not pacman_can_go_right and pacman_vx > 0 and pacman_newaxis_x >= (pacman_new_x - 0.5) * wall.width:
-        pacman_vx = 0
-    if not pacman_can_go_left and pacman_vx < 0 and pacman_newaxis_x <= (pacman_new_x - 0.5) * wall.width:
-        pacman_vx = 0
-    pacman.x += pacman_vx * dt
-
-    # Checa condição de colisão de pacman com parede em y
-    if not pacman_can_go_up and pacman_vy < 0 and pacman_newaxis_y <= (pacman_new_y - 0.5) * wall.height:
-        pacman_vy = 0
-    if not pacman_can_go_down and pacman_vy > 0 and pacman_newaxis_y >= (pacman_new_y - 0.5) * wall.height:
-        pacman_vy = 0
-    pacman.y += pacman_vy * dt
+from EatThis.Classes.Player import *
 
 
 print("Hotkeys:")
@@ -236,12 +39,12 @@ maze = Level(walltype, janela)
 
 # Cria o sprite de Blinky e define o número de frames de sua animação.
 # blinky = Sprite("./Sprites/Blinky.png", 8)
-blinky = Player("./Sprites/Blinky.png", 8)
+blinky = Player(janela, maze, "./Sprites/Blinky.png", 8)
 blinky.set_position(janela.width / 2 - maze.half_maze_width + (maze.wall.width * 1.5 - blinky.width / 2),
                     janela.height / 2 - maze.half_maze_height + (maze.wall.height * 1.5 - blinky.height / 2))
 blinky.set_sequence_time(0, 8, 100, True)
 blinky.set_sequence(0, 1, True)
-facing = 'AFK'
+#facing = 'AFK'
 
 # Cria o sprite de Pacman e define o número de frames de sua animação.
 pacman = Enemy(janela, maze, "./Sprites/pacman.png", 8)
@@ -249,12 +52,6 @@ pacman.set_position(janela.width / 2 + maze.half_maze_width - (maze.wall.width *
                     janela.height / 2 + maze.half_maze_height - (maze.wall.height * 1.5 + pacman.height / 2))
 pacman.set_sequence_time(0, 8, 100, True)
 pacman.set_sequence(0, 1, True)
-#pacman_facing = 'AFK'
-#pacman_vx = 0
-#pacman_vy = 0
-#pacman_base_speed = 120
-#pacman_move_list_is_empty = True
-#moves = ""
 
 # Portal_Esquerdo
 portal_esquerdo = Sprite("Sprites/Walls/" + walltype + "/Portal_L.png", 3)
@@ -279,21 +76,6 @@ while True:
     # Leitura de Entradas
 
     dt = janela.delta_time()
-
-    # Mudança de animação de Blinky nas 4 direções cardinais.
-    if blinky.vy < 0 and facing != 'U':
-        facing = 'U'
-        blinky.set_sequence(6, 8, True)
-    if blinky.vy > 0 and facing != 'D':
-        facing = 'D'
-        blinky.set_sequence(4, 6, True)
-    if blinky.vx < 0 and facing != 'L':
-        facing = 'L'
-        blinky.set_sequence(2, 4, True)
-    if blinky.vx > 0 and facing != 'R':
-        facing = 'R'
-        blinky.set_sequence(0, 2, True)
-
 
     # Código para a geração do mapa para testes.
     if not teclado.key_pressed("N") and not teclado.key_pressed("G") and not teclado.key_pressed("M"):
@@ -323,107 +105,15 @@ while True:
 
 
     # Atualiza buffer de inputs
-    buffer += dt
-
-    if teclado.key_pressed("UP"):
-        buffer = 0
-        cmd = 'u'
-
-    if teclado.key_pressed("DOWN"):
-        buffer = 0
-        cmd = 'd'
-
-    if teclado.key_pressed("RIGHT"):
-        buffer = 0
-        cmd = 'r'
-
-    if teclado.key_pressed("LEFT"):
-        buffer = 0
-        cmd = 'l'
-
-    # Coordenadas do blinky em relação ao 0 da fase
-    blinky_newaxis_x = blinky.x - (janela.width / 2 - maze.half_maze_width) + blinky.width / 2
-    blinky_newaxis_y = blinky.y - (janela.height / 2 - maze.half_maze_height) + blinky.height / 2
-
-    # Versão discretizada das coordenadas do blinky com ajuste (+1) para correspondencia a matriz "level".
-    new_x = blinky_newaxis_x // maze.wall.width + 1
-    new_y = blinky_newaxis_y // maze.wall.height + 1
-
-    can_go_down = maze.level[int(new_y + 1)][int(new_x)] == 0
-    can_go_up = maze.level[int(new_y - 1)][int(new_x)] == 0
-    can_go_left = maze.level[int(new_y)][int(new_x - 1)] == 0
-    can_go_right = maze.level[int(new_y)][int(new_x + 1)] == 0
-
-    # ia do pacman baseada na posição relativa do blinky em relação ao pacman
-    #ia_pacman1()
-
-    # ia do pacman baseado on algoritmo breadth first, mas 'truncado' para 12 comandos por vez (TRAVA O PROGRAMA)
-    #ia_pacman2()
-
-    # Determina as tolerâncias de movimento (até quantos pixels errados blinky aceita para fazer curva)
-    delta_x = 1
-    delta_y = 1
-    x_window = (new_x - 0.5) * maze.wall.width - delta_x < blinky_newaxis_x < (new_x - 0.5) * maze.wall.width + delta_x
-    y_window = (new_y - 0.5) * maze.wall.height - delta_y < blinky_newaxis_y < (new_y - 0.5) * maze.wall.height + delta_y
-    # Condição para aceitar qualquer input de movimento.
-    if buffer < 0.5:
-        # Movimento VERTICAL (REQUERIMENTO DE POSIÇÃO HORIZONTAL)
-        if x_window:
-            if cmd == 'd' and can_go_down:
-                cmd = ''
-                blinky.vx = 0
-                blinky.vy = blinky.base_speed
-            if cmd == 'u' and can_go_up:
-                cmd = ''
-                blinky.vx = 0
-                blinky.vy = -blinky.base_speed
-
-        # Movimento HORIZONTAL (REQUERIMENTO DE POSIÇÃO VERTICAL)
-        if y_window:
-            if cmd == 'r' and can_go_right:
-                cmd = ''
-                blinky.vx = blinky.base_speed
-                blinky.vy = 0
-            if cmd == 'l' and can_go_left:
-                cmd = ''
-                blinky.vx = -blinky.base_speed
-                blinky.vy = 0
-
-    # Para debug (S to stop)
-    if teclado.key_pressed("P"):
-        blinky.vx = blinky.vy = 0
-
-    # TODO: As vezes blinky anda demais antes de sua velocidade ser reduzida a zero,
-    #  similar ao problema de deslizamento do pong. Este problema foi remediado colocando a atualização de posição
-    #  após o check de reset e devido as velocidades baixas, mas caso a velocidade suba acima de 350 o problema retorna.
-    #  Velocidades acima de 350 são ruins de jogar, então o problema foi temporariamente ignorado.
-    # Checa condição de colisão de blinky com parede em x
-    if not can_go_right and blinky.vx > 0 and blinky_newaxis_x >= (new_x - 0.5) * maze.wall.width:
-        blinky.vx = 0
-    if not can_go_left and blinky.vx < 0 and blinky_newaxis_x <= (new_x - 0.5) * maze.wall.width:
-        blinky.vx = 0
-    # Move blinky de acordo com sua velocidade no eixo x
+    blinky.buffer += dt
+    
+    blinky.move1()
     blinky.x += blinky.vx * dt
-
-    # Checa condição de colisão de blinky com parede em y
-    if not can_go_up and blinky.vy < 0 and blinky_newaxis_y <= (new_y - 0.5) * maze.wall.height:
-        blinky.vy = 0
-    if not can_go_down and blinky.vy > 0 and blinky_newaxis_y >= (new_y - 0.5) * maze.wall.height:
-        blinky.vy = 0
-    # Move blinky de acordo com sua velocidade no eixo y
     blinky.y += blinky.vy * dt
 
     pacman.move1(blinky)
     pacman.x += pacman.vx * dt
     pacman.y += pacman.vy * dt
-
-    # Checa colisão de blinky com portal esquerdo.
-    if blinky_newaxis_x < 0 + maze.wall.width / 2:  # aka: 0 + 20/2 = 10
-        blinky.x += 2 * maze.half_maze_width - maze.wall.width
-
-    # Checa colisão de blinky com portal direito.
-    if blinky_newaxis_x > 28 * maze.wall.width - maze.wall.width / 2:  # aka: 28*20 - 20/2 550
-        blinky.x -= 2 * maze.half_maze_width - maze.wall.width
 
     # FPS
     tempo += dt
@@ -444,5 +134,5 @@ while True:
     portal_esquerdo.update()
     portal_direito.draw()
     portal_direito.update()    
-    janela.draw_text("pacman_cmd: " + str(pacman_cmd), 30, 30, 30, color=(255,0,0))
+    janela.draw_text("pacman_cmd: " + str(pacman.cmd), 30, 30, 30, color=(255,0,0))
     janela.update()
