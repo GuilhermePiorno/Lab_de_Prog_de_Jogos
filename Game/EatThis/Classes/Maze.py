3c6cdf45da45242315579261c6c9a88d4616b5f6
GuilhermePiorno <50006421+GuilhermePiorno@users.noreply.github.com>
2023-06-04 18:20:12 -0300

    Update Maze.py
    Ajuste da quantidade de pontos.

--- Game/EatThis/Classes/Maze.py
+++ Game/EatThis/Classes/Maze.py
@@ -34,7 +34,7 @@ class Maze:
         self.half_maze_width = (self.wall.width * 28) / 2
         createlevel()
         self.level = self.fill_level()
-        self.powerup_num = 10
+        self.powerup_num = 4
         self.create_powerups()
         self.pathing = create_path_matrix() # Cria uma matriz com 0s e 1s para auxiliar na criação das sinkmatrix
                                             # level não pode ser usada pois ela contém os sprites das paredes e pontos.
