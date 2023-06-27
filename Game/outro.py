b08cc15aa9d3c0a69686d35de5eff602b1713383
Mauricio Pollis <mauriciopollis@gmail.com>
2023-06-26 21:40:23 -0300

    Rascunho da cena no cockpit

--- Game/outro.py
+++ Game/outro.py
@@ -1,8 +1,75 @@
+# https://br.freepik.com/fotos-vetores-gratis/spaceship-cockpit
+# https://br.freepik.com/vetores-gratis/ponte-de-capitaes-de-espaconave-futura-desenhos-animados-interior-de-posto-de-comando_3685433.htm#query=spaceship%20cockpit&position=13&from_view=keyword&track=ais
+# <a href="https://br.freepik.com/vetores-gratis/ponte-de-capitaes-de-espaconave-futura-desenhos-animados-interior-de-posto-de-comando_3685433.htm#query=spaceship%20cockpit&position=13&from_view=keyword&track=ais">Imagem de vectorpouch</a> no Freepik
+
 from PPlay.window import *
 from PPlay.sprite import *
 from PPlay.gameimage import *
 from PPlay.sound import *
 import random
+
+def cockpit_scene():
+    janela = Window(1080, 720)
+    teclado = janela.get_keyboard()
+
+    cockpit = Sprite("Assets\Sprites\Outro\cockpit_1080_720(1)_sem_janela_3.png", 3)
+    cockpit.set_sequence_time(0, 2, 800, True)
+    fundo = GameImage("Assets\Sprites\Intro\Asset_Sky_Extended.png")
+    poeiras = [Sprite("Assets\Sprites\Intro\Asset_Dust_Extended.png", 1), Sprite("Assets\Sprites\Intro\Asset_Dust_Extended.png", 1)]
+    poeiras[0].set_position(0, 0)
+    poeiras[1].set_position(poeiras[0].width, 0)
+    estrelas = [Sprite("Assets\Sprites\Intro\Asset_Stars_Extended.png", 1), Sprite("Assets\Sprites\Intro\Asset_Stars_Extended.png", 1)]
+    estrelas[0].set_position(0, 0)
+    estrelas[1].set_position(estrelas[0].width, 0)
+    velocidade_poeira = 1000
+    velocidade_estrelas = 10
+
+    pacman_piloto = Sprite("Assets\Sprites\Outro\pacman_piloto_200.png", 1)
+    pacman_piloto.set_position(650, 450)
+
+    blinky = Sprite("Assets\Sprites\Outro\\blinky_cockpit.png", 2)
+    blinky.set_sequence_time(0, 2, 100, True)
+    blinky.set_position(-blinky.width, janela.height - blinky.height)
+
+    while(not teclado.key_pressed("Q")):
+
+        dt = janela.delta_time()
+
+        # movimenta o blinky
+        if teclado.key_pressed("RIGHT"):
+            blinky.x += 200 * dt
+        if teclado.key_pressed("LEFT"):
+            blinky.x -= 200 * dt
+
+        # muda o conteúdo da tela da nave quando blinky chega perto dela
+        if blinky.x + blinky.width >= janela.width/2:
+            cockpit.set_initial_frame(2)
+            cockpit.set_final_frame(0)
+            cockpit.set_sequence_time(0, 2, 200, True)
+
+        # movimenta as estrelas
+        for estrela in estrelas:
+            estrela.x -= velocidade_estrelas * dt
+            if estrela.x < -estrela.width:
+                estrela.x = estrela.width
+
+        # movimenta a poeira
+        for poeira in poeiras:
+            poeira.x -= velocidade_poeira * dt
+            if poeira.x < -poeira.width:
+                poeira.x = poeira.width
+        
+        fundo.draw()
+        for poeira in poeiras:
+            poeira.draw()
+        for estrela in estrelas:
+            estrela.draw()
+        cockpit.draw()
+        cockpit.update()
+        pacman_piloto.draw()
+        blinky.draw()
+        blinky.update()
+        janela.update()
 
 def play_outro():
     janela = Window(1280, 720)
@@ -17,6 +84,8 @@ def play_outro():
     estrelas[0].set_position(0, 0)
     estrelas[1].set_position(estrelas[0].width, 0)
     velocidade_nave = 500
+    vx_nave = 50
+    vy_nave = 0
     velocidade_poeira = 1000
     velocidade_estrelas = 10
     sair = False
@@ -52,23 +121,25 @@ def play_outro():
             if poeira.x < -poeira.width:
                 poeira.x = poeira.width
 
+        nave.x += vx_nave * dt
+        nave.y += vy_nave * dt
         # movimenta a nave até o meio da tela
         if nave.x + nave.width/2 < janela.width/2:
             if teclado.key_pressed("RIGHT"):
-                nave.x += velocidade_nave * dt
+                vx_nave += 1
             if teclado.key_pressed("LEFT"):
-                nave.x -= velocidade_nave * dt
+                vx_nave -= 1
             if teclado.key_pressed("UP"):
-                nave.y -= velocidade_nave * dt
+                vy_nave -= 1
             if teclado.key_pressed("DOWN"):
-                nave.y += velocidade_nave * dt
+                vy_nave += 1
         else:
             # após a nave chegar ao meio da tela, se espaço for pressionado a nave irá sair da tela pela extremidade direita
             if teclado.key_pressed("SPACE"):
                 sair = True
 
         if sair:
-            nave.x += 2 * velocidade_nave * dt
+            vx_nave += 5
             
         if nave.x > janela.width:
             janela.close()
@@ -108,4 +179,5 @@ def play_outro():
             pacman[0].draw()
         janela.update()
 
+cockpit_scene()
 play_outro()
